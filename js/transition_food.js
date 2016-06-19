f6f0b5ddfb585ef95969cb1acc3596f822c025b1
leo <aleoliu566@gmail.com>
2016-06-20 01:18:53 +0800

    var h

--- js/transition_food.js
+++ js/transition_food.js
@@ -24,19 +24,20 @@ $(window).scroll(function() {
     	$('#myyasu').fadeTo(2000,1);
     }	
 });
-// $(document).ready(function() {
 	var h = $("#firstImg").height();
-	var height = h*6;
-	console.log($("#firstImg").height());
-	console.log(height);
-	$("#container").height(height);
-// });
+	// var height = h*6;
+	// console.log($("#firstImg").height());
+	// console.log(height);
+	// $("#container").height(height);
+
 $(window).resize(function() {
-	var h = $("#firstImg").height();
-	var height = h*6;
-	console.log($("#firstImg").height());
-	$("#container").height(height);
+	h = $("#firstImg").height();
+// 	var height = h*6;
+// 	// console.log($("#firstImg").height());
+// 	$("#container").height(height);
 });
+
+
 // console.log(h);
 // 直線
 // var a = $("<svg width="+"20" + "height="+"300><path d="+"M 100 75 L 100 180"+"stroke-width="+"3"+" stroke=black fill=black></svg>");
